36ff03788a256dd83f287435a2ccb4ebd9c61610
yukiriu <gabrieljalbert@hotmail.ca>
2020-09-30 17:13:55 -0400

    Added simple level selection screen

---
+++ Decked Out/Assets/PlaceCard.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceCard : MonoBehaviour
+{
+	public GameObject cardPrefab;
+	private GameObject card;
+}
---
+++ Decked Out/Assets/Scripts/StageSelection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelection : MonoBehaviour
+{
+    public void PlayStage(string StageName){
+        SceneManager.LoadScene("Game");
+    }
+}
