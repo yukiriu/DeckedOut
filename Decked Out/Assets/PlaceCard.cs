f449eb7bb2d879a0023495026cbe203d15295bf0
yukiriu <gabrieljalbert@hotmail.ca>
2020-09-30 20:02:18 -0400

    plein daffaires

--- Decked Out/Assets/PlaceCard.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlaceCard : MonoBehaviour
-{
-	public GameObject cardPrefab;
-	private GameObject card;
-}
---
+++ Decked Out/Assets/Scripts/PlaceCard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceCard : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject[] cardPrefabs;
+	public Vector2 pos = new Vector3(100, 100, 1);
+
+	public void SpawnCard(){
+		Instantiate(cardPrefabs[Random.Range(0,5)], pos, Quaternion.identity, GameObject.FindGameObjectWithTag("Panel").transform);
+	}
+}
