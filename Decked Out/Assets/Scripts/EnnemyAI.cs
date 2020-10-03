9b9b1eca56fa42be3c2292579e19b6cee41233cb
yukiriu <gabrieljalbert@hotmail.ca>
2020-10-02 23:37:26 -0400

    More Stuff

---
+++ Decked Out/Assets/Scripts/EnnemyAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyAI : MonoBehaviour
+{
+    public float speed;
+    private Waypoints Wpoints;
+    private int waypointIndex;
+
+    void Start(){
+        Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+    }
+
+    void Update(){
+        transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f){
+            if(waypointIndex < Wpoints.waypoints.Length - 1){
+                waypointIndex++;
+            } else{
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Decked Out/Assets/Scripts/PlaceCard.cs
+++ Decked Out/Assets/Scripts/PlaceCard.cs
@@ -6,9 +6,48 @@ public class PlaceCard : MonoBehaviour
 {
 	[SerializeField]
 	private GameObject[] cardPrefabs;
-	public Vector2 pos = new Vector3(100, 100, 1);
+	private bool[,] grid = new bool[3,5];
+	private bool cardPlaced;
+	private int colN;
+	private int rowN;
+	private bool gridFull = false;
+
+	public void Start(){
+		InitGrid();
+	}
+
+	private void InitGrid(){
+		for(int i = 0; i < grid.GetLength(0); i++){
+			for(int j = 0; j < grid.GetLength(1); j++){
+				grid[i,j] = false;
+			}
+		}
+	}
+
+	private bool is2DArrayFull(bool[,] grid){
+		for(int i = 0; i < grid.GetLength(0); i++){
+			for(int j = 0; j < grid.GetLength(1); j++){
+				if(grid[i,j] == false){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 
 	public void SpawnCard(){
-		Instantiate(cardPrefabs[Random.Range(0,5)], pos, Quaternion.identity, GameObject.FindGameObjectWithTag("Panel").transform);
+		cardPlaced = false;
+		Debug.Log("cLICK");
+		do{
+			gridFull = is2DArrayFull(grid);
+			colN = Random.Range(0,5);
+			rowN = Random.Range(0,3);
+			if(!grid[rowN, colN]){
+				Instantiate(cardPrefabs[Random.Range(0,5)], new Vector2(colN * 87 - 175, rowN * -85 + 45), Quaternion.identity, GameObject.FindGameObjectWithTag("Panel").transform);
+				grid[rowN, colN] = true;
+				cardPlaced = true;
+				Debug.Log(cardPlaced);
+			}
+		}while(!cardPlaced && !gridFull);
 	}
 }
