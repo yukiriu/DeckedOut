cb2b9b7604faf308fc83ddde7cdc8c9a841a50ea
nicolasbeaulieu123 <71529871+nicolasbeaulieu123@users.noreply.github.com>
2020-10-08 11:38:48 -0400

    Deck Manager

---
+++ Decked Out/Assets/Scripts/CardsHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardsHandler : MonoBehaviour, IPointerDownHandler
+{
+    public GameObject CardMenu;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        CardMenu.SetActive(true);
+    }
+}
---
+++ Decked Out/Assets/Scripts/PopulateGrid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulateGrid : MonoBehaviour
+{
+    public GameObject prefab;
+    public int numberToCreate;
+
+    void Start()
+    {
+        Populate();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Populate()
+    {
+        GameObject newObj;
+
+        for (int i = 0; i < numberToCreate; i++)
+        {
+            newObj = (GameObject)Instantiate(prefab, transform);
+            newObj.GetComponent<Image>().color = Random.ColorHSV();
+        }
+    }
+}
